ea74eabdb923df8d9d43bcf0b47b0590696764ac
codecraftbusinessdev <codecraft.business.dev@gmail.com>
2024-11-07 19:10:09 -0800

    submission-mail-1

--- index.js
+++ index.js
@@ -1,102 +1,214 @@
 const express = require("express");
+var nodemailer = require('nodemailer');
+const multer = require('multer');
 const app = express();
+const axios = require('axios');
+const cors = require('cors');
+
+
+const fs = require('fs');
+const { PDFDocument, rgb } = require('pdf-lib');
+
 
 app.use(express.json());
-app.use(express.urlencoded({extended : true}));
 
-const admin = require("firebase-admin");
-const credentials = require("./key.json");
+app.use(express.urlencoded({ extended: true }));
 
-admin.initializeApp({
-    credential:admin.credential.cert(credentials)
-});
+app.use(cors());
 
-const db = admin.firestore();
 
 const PORT = process.env.PORT || 8080
 
-app.listen(PORT,()=>{
-    console.log("Server started at port : "+PORT);
+app.listen(PORT, () => {
+    console.log("Server started at port : " + PORT);
 });
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
+
+});
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, 'document.pdf');
+    },
+});
+
+const upload = multer({ storage });
+
+//Offer Letter Email
+app.post('/upload', upload.single('file'), (req, res) => {
+
+    console.log("Request is arrived");
+
+    const email = req.body.email;
+    const name = req.body.name;
+    const track = req.body.track;
+    const duration = req.body.duration;
+    const startdate = req.body.startdate;
+    const enddate = req.body.enddate;
+
+    console.log(email);
+
+    if (!req.file) {
+        return res.status(400).json({ error: 'No file uploaded' });
+    }
+
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'hr.codecraft.infotech@gmail.com',
+            pass: 'gnkdjcrplzbrhcgd'
+        }
+    });
+
+    var mailOptions = {
+        from: 'hr.codecraft.infotech@gmail.com',
+        to: email,
+        subject: 'Congratulations, you have been Selected for the Internship!',
+        html: '<h1 > CODECRAFT INFOTECH </h1> <br><p>Dear ' + name + ',' + '</p><p>We are pleased to inform you that you have been selected for the ' + track +' Internship Program at CodeCraft InfoTech! Congratulations on this exciting achievement!</p> <p>As a '+ track +' intern, you will have the opportunity to gain hands-on experience and develop valuable skills that will prepare you for your future career. Our program is designed to challenge you, support your growth, and provide you with a meaningful and rewarding experience.</P> <P>Enclosed with this email, you will find your Offer Letter. We kindly request that you consult the <a href="https://www.canva.com/design/DAGU8eRlmEY/E_jP0ALiPMNnz0aYutqQSA/view" >Task Lists Document</a> Document to fully understand your assigned roles and responsibilities during the course of your internship.</P><h3>During your internship at CodeCraft InfoTech, please keep in mind the following points:</h3><p>1. It is mandatory that you update your LinkedIn profile and share your Offer Letter on LinkedIn.</p><p>2. Following the completion of each task, it is mandatory that you create a post on LinkedIn outlining the tasks you have completed and the knowledge gained. You may choose to include a video in your post, but it is optional.</p><p>3. It is compulsory that you tag <a href="https://linkedin.com/company/codecraft-infotech">CodeCraftInfoTech</a> and use the hashtag #CodeCraftInfoTech in all posts related to your internship as part of our social media policy.</p><p>4. For submission of your completed tasks, a Task Submission Form will be sent to you by a separate email after 10-15 days from now. You dont need to email your tasks to us.</p><p>5. The internship program is unpaid, and no monetary compensation will be provided. The program offers certificates & LoR upon successful completion of assigned tasks and meeting the program requirements.</p><h1>Timeline</h1><h2>Start Date : '+ startdate +'</h2><h2>End Date : '+ enddate +'</h2><h2>Submission : Submit before '+ enddate +'</h2><br><h2>Once you submit submission form you will receive internship certificate by a mail within 1-2 days.</h2><br><h2><a href="https://www.canva.com/design/DAGU8eRlmEY/E_jP0ALiPMNnz0aYutqQSA/view"><button style="background-color: #008CBA;">Click here to visit your tasks</button></a></h2>',
+        attachments: [
+            {
+                filename: 'Offer_Letter.pdf', // <= Here: made sure file name match
+                path: './uploads/document.pdf', // <= Here
+                contentType: 'application/pdf'
+            }
+        ]
+    };
+
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("success");
+        }
+
+    })
+
+});
+
+
+//Certificate Email
+app.post('/certificatemail', upload.single('file'), (req, res) => {
+
+    console.log("Request is arrived");
+
+    const email = req.body.email;
+    const name = req.body.name;
+    const track = req.body.track;
+    const duration = req.body.duration;
+    const startdate = req.body.startdate;
+    const enddate = req.body.enddate;
+    const certificateno = req.body.certificateno;
+
+    console.log(email);
+
+    if (!req.file) {
+        return res.status(400).json({ error: 'No file uploaded' });
+    }
+
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'hr.codecraft.infotech@gmail.com',
+            pass: 'gnkdjcrplzbrhcgd'
+        }
+    });
+
+    var mailOptions = {
+        from: 'hr.codecraft.infotech@gmail.com',
+        to: email,
+        subject: 'Congratulations, You Successfully Completed Your Internship!',
+        html: '<h1 > CODECRAFT INFOTECH </h1> <br><p>Dear ' + name + ',' + '</p><p>We are thrilled to inform you that you completed Internship Program in ' + track +'  at CodeCraft InfoTech! Congratulations on this exciting achievement!</p> <p>The internship is scheduled from ' +  startdate + ' and concluded on the ' + enddate +' resulting in a 1 Month duration for the program.</p> <br> <h2> Your Certificate NO : '+ certificateno +'</h2> <p>Your can verify your ceritificate <a href="https://codecraftinfotech.netlify.app/verify_certificate">here</a></p><br><p>Thank you, Team CODECRAFT INFOTECH.</p>',
+        attachments: [
+            {
+                filename: 'Internship_Certificate.pdf', // <= Here: made sure file name match
+                path: './uploads/document.pdf', // <= Here
+                contentType: 'application/pdf'
+            }
+        ]
+    };
+
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("success");
+        }
+
+    })
+
+});
+
+//send submission form mail
+
+app.post('/submissionmail',(req, res) => {
     
-    res.send("Hello i am active");
-    
-})
 
-app.post("/addFeedback",async(req,res)=>{
+    console.log("Request is arrived");
+
+    const email = req.body.email;
+    const name = req.body.name;
+    const enddate = req.body.enddate;
+
+    console.log(email);
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'hr.codecraft.infotech@gmail.com',
+            pass: 'gnkdjcrplzbrhcgd'
+        }
+    });
+
+    var mailOptions = {
+        from: 'hr.codecraft.infotech@gmail.com',
+        to: email,
+        subject: 'Internship Submission Form Link for CodeCraft Infotech',
+        html: '<h1 > CODECRAFT INFOTECH </h1> <br><p>Dear ' + name + ',' + '</p> <p>Greetings! We hope this email finds you in good health. We wanted to remind you that the <a href="https://docs.google.com/forms/d/e/1FAIpQLSd5tT6S2TquQRdonSopo3VVskxKkgUr7N2DqJYrFkE9TQMe7Q/viewform?usp=sf_link" >Internship Submission Form</a> is open and available for all students to submit their internship work.</p> <p>Please ensure timely submission of your <a href="https://www.canva.com/design/DAGU8eRlmEY/E_jP0ALiPMNnz0aYutqQSA/view?utm_content=DAGU8eRlmEY&utm_campaign=designshare&utm_medium=link&utm_source=viewer" >Internship Tasks</a> in order to be eligible for the Verified Certificate of Completion.</p> <br><h2><a href="https://docs.google.com/forms/d/e/1FAIpQLSd5tT6S2TquQRdonSopo3VVskxKkgUr7N2DqJYrFkE9TQMe7Q/viewform?usp=sf_link"><button style="background-color: #008CBA;">Click here to open submmision form</button></a></h2><p>Please ensure timely submission of your work in order to be eligible for the Verified Certificate. Late submissions will result in a delay in the release of the certificate.<p/><p>The deadline for submission is 11:59 PM IST on ' + enddate +'.</p><p>We appreciate your hard work and dedication to this internship program.</p>'
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log("success");
+        }
+
+    })
+});
+
+
+app.post('/pdf', async (req, res) => {
+    console.log("request arrived");
+
+    const pdfUri = req.body.pdfUri;
+
+    console.log(pdfUri);
     try {
-        const feedbackJson = {
-            name : req.body.name,
-            email : req.body.email,
-            subject : req.body.subject,
-            message : req.body.message
-        };
+        // Fetch the PDF from the URI
+        const response = await axios.get(pdfUri, {
+            responseType: 'arraybuffer', // Important for binary data
+        });
 
-        const result = await db.collection("feedback").add(feedbackJson);
-        res.send(result)
-        
-        console.log("requested addFeedback");
-
+        // Save the PDF to a file
+        fs.writeFileSync("downloaded.pdf", response.data);
+        console.log(`PDF saved successfully as downloaded.pdf`);
     } catch (error) {
-        res.send(error)
+        console.error('Error saving PDF:', error);
     }
-})
 
-app.get("/getAllFeedback",async(req,res)=>{
-    try {
-        
-        const userRef = db.collection("feedback");
-        const result = await userRef.where("name", "==", "jone").get();
+    res.send(pdfUri);
 
-        let responseArr = [];
-
-        result.forEach(doc=>{
-            responseArr.push(doc.data());
-        })
-
-        console.log("requested");
-        res.send(responseArr);
-    } catch (error) {
-        res.send(error)
-    }
-})
-
-app.post("/addCertificate",async(req,res)=>{
-    try {
-        const certificateJson = {
-            certificate_no : req.body.certificate_no,
-            name : req.body.name,
-            mobileNo : req.body.mobileNo,
-            email : req.body.email,
-            college : req.body.college,
-            internship_duration : req.body.internship_duration,
-            internship_track : req.body.internship_track,
-            internship_status : "running",
-            start_date : req.body.start_date,
-            end_date : req.body.end_date,
-            issue_date : "pending",
-        };
-
-        const result = await db.collection("certificate").doc(certificateJson.certificate_no).set(certificateJson);
-        res.send(result)
-    } catch (error) {
-        res.send(error)
-    }
-})
-
-app.get("/verifyCertificate/:certificate_id",async(req,res)=>{
-    try {
-        
-        const userRef = db.collection("certificate").doc(req.params.certificate_id);
-        const result = await userRef.get();
-        res.send(result.data());
-
-        console.log("requested verifyCertificate");
+});
 
 
-    } catch (error) {
-        res.send(error)
-    }
-})
+
+
